76b4d9146dd40beee9ec099dff6c8685cc696b25
Norlan Tibanear <ntibanear@gmail.com>
2020-09-30 22:07:15 -0400

    Done

---
+++ Astronomy/Helpers/URLSession+NetworkDataLoader.swift
@@ -0,0 +1,9 @@
+//
+//  URLSession+NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Norlan Tibanear on 9/29/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ AstronomyTests/MockJSON.swift
@@ -0,0 +1,9 @@
+//
+//  MockJSON.swift
+//  AstronomyTests
+//
+//  Created by Norlan Tibanear on 9/29/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
